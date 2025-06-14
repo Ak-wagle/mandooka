d3cd1f8052cada1687eaf5d8239a82906a1a8019
Aniruddha Wagle K <aniruddhawagle@gmail.com>
2025-06-14 15:26:46 +0530

    minor fix related to gazebo launch

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -8,7 +8,7 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 from launch_ros.actions import Node
 
-from launch.actions import ExecuteProcess
+# from launch.actions import ExecuteProcess
 
 def generate_launch_description():
 
@@ -20,12 +20,13 @@ def generate_launch_description():
                 )]), launch_arguments={'use_sim_time': 'true'}.items()
     )
 
-    # # Gazebo launch file, provided by the gazebo_ros package
-    # gazebo = IncludeLaunchDescription(
-    #             PythonLaunchDescriptionSource([os.path.join(
-    #                 get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-    #          )
+    # Gazebo launch file, provided by the gazebo_ros package
+    gazebo = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+             )
 
+    # if gazebo doesn't open --> source /usr/share/gazebo/setup.bash
 
     # # explicitly pass the factory plugin for gazzebo to load the factory plugin
     # gazebo = IncludeLaunchDescription(
@@ -36,13 +37,11 @@ def generate_launch_description():
     #     'extra_gazebo_args': '--verbose -s libgazebo_ros_factory.so'
     # }.items())
 
-
-    # Explicitly passing the factory plugin
-    gazebo = ExecuteProcess(
-        cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_factory.so'],
-        output='screen'
-    )
-
+    # # Explicitly passing the factory plugin
+    # gazebo = ExecuteProcess(
+    #     cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_factory.so'],
+    #     output='screen'
+    # )
 
     # Spawner node from the gazebo_ros package. The entity name matters only if you have multiple robots.
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
