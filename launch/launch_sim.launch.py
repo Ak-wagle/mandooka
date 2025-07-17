a48b729a304643cfd70b867ee51f06b7868dd7c9
Aniruddha Wagle K <aniruddhawagle@gmail.com>
2025-07-17 16:00:38 +0530

    migration to new-gaz ign fortress

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -3,9 +3,9 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 # from launch.actions import ExecuteProcess
@@ -14,7 +14,7 @@ def generate_launch_description():
 
     package_name='mandooka'
 
-    gazebo_params_file = os.path.join(get_package_share_directory(package_name),'config','gazebo_params.yaml')
+    # gazebo_params_file = os.path.join(get_package_share_directory(package_name),'config','gazebo_params.yaml')
 
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
@@ -35,12 +35,25 @@ def generate_launch_description():
             parameters=[twist_mux_params],
             remappings=[('/cmd_vel_out','/diff_cont/cmd_vel_unstamped')]
         )
+    default_world = os.path.join(
+        get_package_share_directory(package_name),
+        'worlds',
+        'empty.world'
+        )    
 
-    # Gazebo launch file, provided by the gazebo_ros package
+    world = LaunchConfiguration('world')
+
+    world_arg = DeclareLaunchArgument(
+        'world',
+        default_value=default_world,
+        description='World to load'
+        )
+
+    # Gazebo launch file, provided by the ros_gz_sim package
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-                    launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
+                    get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')]),
+                    launch_arguments={'gz_args': ['-r -v4', world], 'on_exit_shutdown': 'true'}.items()
              )
 
     # if gazebo doesn't open --> source /usr/share/gazebo/setup.bash
@@ -60,10 +73,11 @@ def generate_launch_description():
     #     output='screen'
     # )
 
-    # Spawner node from the gazebo_ros package. The entity name matters only if you have multiple robots.
-    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+    # Spawner node from the ros_gz_sim package. The entity name matters only if you have multiple robots.
+    spawn_entity = Node(package='ros_gz_sim', executable='create',
                         arguments=['-topic', 'robot_description',
-                                   '-entity', 'mandooka'],
+                                   'name', 'mandooka',
+                                   '-z', '0.1'],
                         output='screen')
     
     diff_drive_spawner = Node(
@@ -78,12 +92,33 @@ def generate_launch_description():
         arguments=["joint_broad"],
     )
 
+    bridge_params = os.path.join(get_package_share_directory(package_name),'config','gz_bridge.yaml')
+    
+    ros_gz_bridge = Node(
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
+        arguments=[
+            '--ros-args',
+            '-p',
+            f'config_file:={bridge_params}',
+        ]
+    )
+
+    ros_gz_image_bridge = Node(
+        package="ros_gz_image",
+        executable="image_bridge",
+        arguments=["/camera/image_raw"]
+    )
+
     return LaunchDescription([
         rsp,
         joystick,
         twist_mux,
+        world_arg,
         gazebo,
         spawn_entity,
         diff_drive_spawner,
         joint_broad_spawner,
+        ros_gz_bridge,
+        ros_gz_image_bridge
     ])
